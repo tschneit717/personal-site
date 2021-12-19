5473ae3f81e45ef8ffe41402645cb97f5ef62488
Thomas Schneider <tschneit717@gmail.com>
2021-12-19 13:49:29 -0500

    Overhaul again lol

--- web/gatsby-node.js
+++ web/gatsby-node.js
@@ -1,15 +1,17 @@
-const {isFuture} = require('date-fns')
+const { isFuture } = require("date-fns");
 /**
  * Implement Gatsby's Node APIs in this file.
  *
  * See: https://www.gatsbyjs.org/docs/node-apis/
  */
 
-async function createProjectPages (graphql, actions) {
-  const {createPage} = actions
+async function createProjectPages(graphql, actions) {
+  const { createPage } = actions;
   const result = await graphql(`
     {
-      allSanitySampleProject(filter: {slug: {current: {ne: null}}, publishedAt: {ne: null}}) {
+      allSanitySampleProject(
+        filter: { slug: { current: { ne: null } }, publishedAt: { ne: null } }
+      ) {
         edges {
           node {
             id
@@ -21,27 +23,27 @@ async function createProjectPages (graphql, actions) {
         }
       }
     }
-  `)
+  `);
 
-  if (result.errors) throw result.errors
+  if (result.errors) throw result.errors;
 
-  const projectEdges = (result.data.allSanitySampleProject || {}).edges || []
+  const projectEdges = (result.data.allSanitySampleProject || {}).edges || [];
 
   projectEdges
     .filter(edge => !isFuture(edge.node.publishedAt))
     .forEach(edge => {
-      const id = edge.node.id
-      const slug = edge.node.slug.current
-      const path = `/project/${slug}/`
+      const id = edge.node.id;
+      const slug = edge.node.slug.current;
+      const path = `/project/${slug}/`;
 
       createPage({
         path,
-        component: require.resolve('./src/templates/project.js'),
-        context: {id}
-      })
-    })
+        component: require.resolve("./src/templates/project.js"),
+        context: { id }
+      });
+    });
 }
 
-exports.createPages = async ({graphql, actions}) => {
-  await createProjectPages(graphql, actions)
-}
+exports.createPages = async ({ graphql, actions }) => {
+  await createProjectPages(graphql, actions);
+};
---
+++ web/src/pages/index.tsx
@@ -0,0 +1,13 @@
+import * as React from "react";
+
+const IndexPage = () => {
+  return (
+    <main>
+      <title>Title</title>
+      <h1>Welcome</h1>
+      <p>Hello world</p>
+    </main>
+  );
+};
+
+export default IndexPage;
